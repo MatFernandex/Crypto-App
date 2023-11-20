614745e016f8c03105200cae74e3354ae604bde6
MatFernandex <109188991+MatFernandex@users.noreply.github.com>
2023-11-19 21:27:41 -0300

    Crypto-App

---
+++ po23s-cch-cripto/src/main/java/po23s/Po23sCchCripto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package po23s;
+
+import po23s.http.PainelCrip;
+
+public class Po23sCchCripto {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                PainelCrip painel = new PainelCrip();
+                painel.setVisible(true);
+            }
+        });
+    }
+}
+
+
